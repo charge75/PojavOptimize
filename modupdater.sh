80ac53a7d09f1c5c70c2c67cecfaa7f3c93f9577
Adam Tunnic <atunnic@outlook.com>
2021-03-27 13:48:31 -0400

    Disable workflow temporarily

--- modupdater.sh
+++ modupdater.sh
@@ -4,16 +4,12 @@
 # It's not for the end-users and will fail if not used properly.
 
 # STILL IN TETS MODE
-
-# Make the directory and change to it
-mkdir git-temp && cd git-temp
-
-# Clone the source repository
-git clone https://github.com/Spottedleaf/Starlight.git
-
-# Change to the source repo dir
-cd Starlight
+$VERSION
 
 # Build!
+cd git-temp/Starlight
 chmod +x gradlew
 ./gradlew build
+
+# Move the target jars out of the build directory
+mv build/libs/starlight-fabric-$VERSION-$SHA.jar ../../starlight.jar
