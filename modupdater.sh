fe38149d23211df57e5ce633dc9c1393629e90fc
Adam Tunnic <atunnic@outlook.com>
2021-03-27 12:18:55 -0400

    Added test workflow file

--- modupdater.sh
+++ modupdater.sh
@@ -3,4 +3,7 @@
 # This script is intended for use by the GitHub workflow file on the repository.
 # It's not for the end-users and will fail if not used properly.
 
+# Checking to see if it's all good.
+echo Good
+
 
