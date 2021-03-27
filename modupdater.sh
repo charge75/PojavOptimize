887316cd6c11c21e27b2f347d6c8d00246868291
Adam Tunnic <atunnic@outlook.com>
2021-03-27 12:48:56 -0400

    Run test build, using Starlight

--- modupdater.sh
+++ modupdater.sh
@@ -3,7 +3,16 @@
 # This script is intended for use by the GitHub workflow file on the repository.
 # It's not for the end-users and will fail if not used properly.
 
-# Checking to see if it's all good.
-echo Good
+# STILL IN TETS MODE
 
+# Make the directory and change to it
+mkdir git-temp && cd git-temp
 
+# Clone the source repository
+git clone https://github.com/Spottedleaf/Starlight.git
+
+# Change to the source repo dir
+cd Starlight
+
+# Build!
+./gradlew build
